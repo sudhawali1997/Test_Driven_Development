4ee295e74dcf22b87101094b88a024d4b9f82698
Sudha Wali <sudhawali02@gmail.com>
2024-10-26 12:41:49 +0530

    Test case passed for adding customeDelimeter

--- src/main/java/com/in/Test_Driven_Development/StringCalculator.java
+++ src/main/java/com/in/Test_Driven_Development/StringCalculator.java
@@ -1,5 +1,8 @@
 package com.in.Test_Driven_Development;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 	public int add(String numbers) {
@@ -7,14 +10,22 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		String[] numArray=numbers.split("[,\n]");
+		String delimeter=",|\n";
+	    if (numbers.startsWith("//")) {
+	        int delimiterIndex = numbers.indexOf("\n");
+	        delimeter = numbers.substring(2, delimiterIndex);
+	        numbers = numbers.substring(delimiterIndex + 1);
+	    }
+		String[] numArray=numbers.split(delimeter);
 		int sum=0;
+		
 		for (String num : numArray) {
-	        sum += Integer.parseInt(num);
+            sum += Integer.parseInt(num);
+	       
 	    }
 	    return sum;
 	}
-		//return Integer.parseInt(numbers);
+		
 	}
 
 
