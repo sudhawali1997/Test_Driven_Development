4b5a6d80b5853d8d5fc26122fb169a2a0cd5edfd
Sudha Wali <sudhawali02@gmail.com>
2024-10-26 11:30:41 +0530

    first commit

---
+++ src/main/java/com/in/Test_Driven_Development/TestDrivenDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.in.Test_Driven_Development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDrivenDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in/Test_Driven_Development/TestDrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.Test_Driven_Development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
