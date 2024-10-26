053795ce0524ed0556f8e74038735ede4ef61e26
Sudha Wali <sudhawali02@gmail.com>
2024-10-26 11:42:33 +0530

    Test case pass for empty String

--- src/main/java/com/in/Test_Driven_Development/StringCalculator.java
+++ src/main/java/com/in/Test_Driven_Development/StringCalculator.java
@@ -1,5 +1,10 @@
 package com.in.Test_Driven_Development;
 
 public class StringCalculator {
-
+	
+	public int add(String numbers) {
+        return 0;
+    }
 }
+
+
--- src/test/java/com/in/Test_Driven_Development/TestDrivenDevelopmentApplicationTests.java
+++ src/test/java/com/in/Test_Driven_Development/TestDrivenDevelopmentApplicationTests.java
@@ -1,5 +1,7 @@
 package com.in.Test_Driven_Development;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
