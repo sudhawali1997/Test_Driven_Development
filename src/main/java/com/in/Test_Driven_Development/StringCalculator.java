f643a0c7062ac705b9a6d487b008261692c9642b
Sudha Wali <sudhawali02@gmail.com>
2024-10-26 12:09:01 +0530

    Test case passes addWithNewLineDelimter

--- src/main/java/com/in/Test_Driven_Development/StringCalculator.java
+++ src/main/java/com/in/Test_Driven_Development/StringCalculator.java
@@ -7,7 +7,7 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		String[] numArray=numbers.split(",");
+		String[] numArray=numbers.split("[,\n]");
 		int sum=0;
 		for (String num : numArray) {
 	        sum += Integer.parseInt(num);
